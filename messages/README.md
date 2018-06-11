893c86878ff2dea053009e34371939f8e5a06b76
Kelly Tattersall <kellytattersall@Kellys-MacBook-Air-2.local>
2018-06-11 19:41:44 -0400

    Load the data from the data.json file

---
+++ messages/README.md
@@ -0,0 +1,11 @@
+# comp20-ktattersall
+
+Lab 9: Messages (Part 2)
+
+To my knowledge, all aspects of this work have been correctly implemented.
+
+I have neither collaborated nor discussed this assignment with anyone.
+
+I spent approximately _ hours completing this assignment.
+
+is it possible to request the data from a different origin (e.g., http://messagehub.herokuapp.com/) or from your local machine (from file:///) from using XMLHttpRequest? Why or why not?
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here...
+/* lab.js */
+function parse() {
+        var http_req = new XMLHttpRequest();
+        http_req.open("GET", "data.json", true);
+        http_req.send(null);
+
+        http_req.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                        var obj = JSON.parse(http_req.responseText);
+                        document.getElementById("messages").innerHTML =
+                                obj[0].content + " (" + obj[0].username +
+                                ")<br>" + obj[1].content + " (" +
+                                obj[1].username + ")";
+                }
+        };
+}
