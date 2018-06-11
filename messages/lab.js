2dafda34e9d092462df54a6b381fb4d60f4a7c1b
Kelly Tattersall <kellytattersall@Kellys-MacBook-Air-2.local>
2018-06-11 19:50:41 -0400

    Revert to original (data.json)

--- messages/lab.js
+++ messages/lab.js
@@ -1,11 +1,11 @@
 /* lab.js */
 function parse() {
         var http_req = new XMLHttpRequest();
-        http_req.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+        http_req.open("GET", "data.json", true);
         http_req.send(null);
 
         http_req.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
+                if (http_req.readyState == 4 && http_req.status == 200) {
                         var obj = JSON.parse(http_req.responseText);
                         document.getElementById("messages").innerHTML =
                                 obj[0].content + " (" + obj[0].username +
